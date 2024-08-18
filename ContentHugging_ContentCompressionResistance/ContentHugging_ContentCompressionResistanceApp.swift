bd23d78aedfe4707680554f46036cefd4127fcc8
Cong Le <longchike@gmail.com>
2024-08-17 23:08:08 -0700

    initial setup

---
+++ ContentHugging_ContentCompressionResistance/ContentHugging_ContentCompressionResistanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentHugging_ContentCompressionResistanceApp.swift
+//  ContentHugging_ContentCompressionResistance
+//
+//  Created by Cong Le on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ContentHugging_ContentCompressionResistanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentHugging_ContentCompressionResistance/UIKitViewController.swift
@@ -0,0 +1,8 @@
+//
+//  UIKitViewController.swift
+//  ContentHugging_ContentCompressionResistance
+//
+//  Created by Cong Le on 8/17/24.
+//
+
+import Foundation
